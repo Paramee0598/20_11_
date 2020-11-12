b96aee67bfe0fc7312e744b99652f661851c4847
paramee0598 <63010598@kmitl.co.th>
2020-11-12 15:36:23 +0700

    Add project files.

---
+++ 20_11_/Source.cpp
@@ -0,0 +1,70 @@
+#include<stdio.h>
+
+#define N 3
+
+void enter() {
+
+}
+
+
+int main() {
+
+	struct informacation {
+		char _name[100] = {};
+		char nick_Name[100] = {};
+		int _age = 0;
+		float _weight = 0;
+	};
+	struct informacation I[N];
+
+	printf("Enter the informacation: \n");
+
+	int i = 0;
+	int j = -1;
+
+	for (i = 0; i < N; i++) {
+
+		printf("Name :  ");
+		gets_s(I[i]._name);
+
+		if (j == i) {
+			gets_s(I[i]._name);
+
+		}
+		j = i + 1;
+		printf("NickName :  ");
+		gets_s(I[i].nick_Name);
+
+		printf("Age :  ");
+		scanf_s("%d", &I[i]._age);
+		if ((I[i]._age) < 0) {
+			printf("Error ");
+			return 0;
+		}
+		printf("Weight :  ");
+		scanf_s("%f", &I[i]._weight);
+		if ((I[i]._weight) < 0) {
+			printf("Error ");
+			return 0;
+		}
+		fflush(stdin);
+		printf("\n");
+	}
+	printf("\n\n");
+	printf("===============================================================================\n");
+	printf("Name\t\t\t\tNick Name\t\tAge\tWeight\n");
+	printf("===============================================================================\n");
+
+
+	for (i = 0; i < N; i++) {
+		printf("%s\t\t\t\t", I[i]._name);
+		printf("%-4s\t\t\t", I[i].nick_Name);
+		printf("%d\t", I[i]._age);
+		printf("%.1f\n", I[i]._weight);
+		printf("-------------------------------------------------------------------------------\n");
+
+
+
+	}
+
+}
